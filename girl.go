7e4d30709a17222b1fcb3429cae6ef908c9ce010
cgyy <a1634286@163.com>
2013-05-24 17:19:38 +0800

    reformat

--- girl.go
+++ girl.go
@@ -4,13 +4,13 @@ import (
 	"encoding/json"
 	"html/template"
 	"io/ioutil"
-    "log"
+	"log"
 	"net/http"
 	"os"
-    "path"
+	"path"
 	"path/filepath"
+	"runtime/debug"
 	"strings"
-    "runtime/debug"
 )
 
 type Invoker func(c *Context) View
@@ -18,7 +18,7 @@ type Invoker func(c *Context) View
 type Context struct {
 	Request *http.Request
 	params  map[string]string
-    vars map[string]string
+	vars    map[string]string
 	http.ResponseWriter
 }
 
@@ -39,7 +39,7 @@ type View interface {
 
 type TemplateView struct {
 	Template *template.Template
-    Data interface{}
+	Data     interface{}
 }
 
 type JSONView struct {
@@ -97,215 +97,210 @@ func (c *Context) Redirect(url string) View {
 }
 
 func (c *Context) GetParam(s string) string {
-    if c.params == nil {
-        c.params = make(map[string]string)
-        err := c.Request.ParseForm()
-        if err == nil {
-            for k, v := range c.Request.Form {
-                c.params[k] = v[0]
-            }
-        }
+	if c.params == nil {
+		c.params = make(map[string]string)
+		err := c.Request.ParseForm()
+		if err == nil {
+			for k, v := range c.Request.Form {
+				c.params[k] = v[0]
+			}
+		}
 
-        if c.vars != nil {
-            for k, v := range c.vars {
-                c.params[k] = v
-            }
-        }
+		if c.vars != nil {
+			for k, v := range c.vars {
+				c.params[k] = v
+			}
+		}
 
-    }
-    return c.params[s]
+	}
+	return c.params[s]
 }
 
 func (c *Context) invoke(routeHander Invoker) (err error) {
-    defer func() {
-        if e := recover(); e != nil {
-            err = e.(error)
-        }
-    }()
+	defer func() {
+		if e := recover(); e != nil {
+			err = e.(error)
+		}
+	}()
 
-    view := routeHander(c)
-    if view != nil {
-        err = view.Apply(c.ResponseWriter, c.Request)
-    }
-    return
+	view := routeHander(c)
+	if view != nil {
+		err = view.Apply(c.ResponseWriter, c.Request)
+	}
+	return
 }
 
-
 func Get(pattern string, invoker Invoker) {
-    addRoute(pattern, "GET", invoker)
-    addRoute(pattern, "HEAD", invoker)
+	addRoute(pattern, "GET", invoker)
+	addRoute(pattern, "HEAD", invoker)
 }
 
 func Post(pattern string, invoker Invoker) {
-    addRoute(pattern, "POST", invoker)
+	addRoute(pattern, "POST", invoker)
 }
 
 func Put(pattern string, invoker Invoker) {
-    addRoute(pattern, "PUT", invoker)
+	addRoute(pattern, "PUT", invoker)
 }
 
 func Delete(pattern string, invoker Invoker) {
-    addRoute(pattern, "DELETE", invoker)
+	addRoute(pattern, "DELETE", invoker)
 }
 
 func addRoute(pattern, method string, invoker Invoker) {
-    signature := girl.routes[method]
-    girl.routes[method] = append(signature, Route{pattern, invoker})
+	signature := girl.routes[method]
+	girl.routes[method] = append(signature, Route{pattern, invoker})
 }
 
+func matchUrl(scheme, path string) (bool, map[string]string) {
+	schemeList := strings.Split(scheme, "/")
+	pathList := strings.Split(path, "/")
 
-func matchUrl(scheme , path string) (bool, map[string]string) {
-    schemeList := strings.Split(scheme, "/")
-    pathList := strings.Split(path, "/")
+	if len(schemeList) != len(pathList) {
+		return false, nil
+	}
 
-    if len(schemeList) != len(pathList) {
-        return false, nil
-    }
+	var vars map[string]string
 
-    var vars map[string]string
+	for i, s := range schemeList {
+		p := pathList[i]
+		if s != p {
+			if len(s) > 1 && s[0] == ':' {
+				if vars == nil {
+					vars = make(map[string]string)
+				}
+				vars[s[1:]] = p
+			} else {
+				return false, nil
+			}
+		}
+	}
 
-    for i, s := range schemeList {
-        p := pathList[i]
-        if s != p {
-            if len(s) > 1 && s[0] == ':' {
-                if vars ==  nil {
-                    vars = make(map[string]string)
-                }
-                vars[s[1:]] = p
-            } else {
-                return false, nil
-            }
-        }
-    }
-
-    return true, vars
+	return true, vars
 }
-
 
 func (g *Girl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    rPath := r.URL.Path
+	rPath := r.URL.Path
 
-    if rPath == "/favicon.ico" {
-        http.ServeFile(w, r, g.RootDir+"/public"+rPath)
-        return
-    }
+	if rPath == "/favicon.ico" {
+		http.ServeFile(w, r, g.RootDir+"/public"+rPath)
+		return
+	}
 
-    if strings.HasPrefix(rPath, "/public/") {
-        http.ServeFile(w, r, g.RootDir+rPath)
-        return
-    }
+	if strings.HasPrefix(rPath, "/public/") {
+		http.ServeFile(w, r, g.RootDir+rPath)
+		return
+	}
 
+	log.Println(rPath)
+	context := Context{r, nil, nil, w}
+	for _, router := range girl.routes[r.Method] {
+		scheme := router.pattern
 
-    log.Println(rPath)
-    context := Context{r, nil, nil, w}
-    for _, router := range girl.routes[r.Method] {
-        scheme := router.pattern
+		isMatch, vars := matchUrl(scheme, rPath)
+		if isMatch {
+			context.vars = vars
+			err := context.invoke(router.invoker)
 
-        isMatch, vars := matchUrl(scheme, rPath)
-        if isMatch {
-            context.vars = vars
-            err := context.invoke(router.invoker)
+			if err != nil {
+				stack := string(debug.Stack())
+				log.Println(stack)
+				context.Abort(http.StatusInternalServerError, "internal server error\n\n\n"+stack)
+			}
+			return
+		}
+	}
 
-            if err != nil {
-                stack := string(debug.Stack())
-                log.Println(stack)
-                context.Abort(http.StatusInternalServerError, "internal server error\n\n\n" + stack)
-            }
-            return
-        }
-    }
-
-    context.Abort(http.StatusNotFound, "not found")
+	context.Abort(http.StatusNotFound, "not found")
 }
 
 func (g *Girl) initTemplate() error {
-    baseDir := path.Join(g.RootDir, "views")
-    g.templateSet = template.New(baseDir)
+	baseDir := path.Join(g.RootDir, "views")
+	g.templateSet = template.New(baseDir)
 
-    if !dirExists(baseDir) {
-        log.Println("folder\"", baseDir, "\" not exists!\n")
-        return nil
-    }
+	if !dirExists(baseDir) {
+		log.Println("folder\"", baseDir, "\" not exists!\n")
+		return nil
+	}
 
-    filepath.Walk(baseDir, func(path string, info os.FileInfo, err error) error {
-        if !info.IsDir() && !strings.HasPrefix(filepath.Base(path), ".") {
+	filepath.Walk(baseDir, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() && !strings.HasPrefix(filepath.Base(path), ".") {
 
-            b, err := ioutil.ReadFile(path)
-            if err != nil {
-                return err
-            }
-            s := string(b)
+			b, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			s := string(b)
 
-            tmplName := path[len(baseDir)+1:]
-            tmplName = strings.Split(tmplName, ".")[0]
-            tmpl := g.templateSet.New(tmplName)
+			tmplName := path[len(baseDir)+1:]
+			tmplName = strings.Split(tmplName, ".")[0]
+			tmpl := g.templateSet.New(tmplName)
 
-            _, err = tmpl.Parse(s)
+			_, err = tmpl.Parse(s)
 
-            if err != nil {
-                return err
-            }
+			if err != nil {
+				return err
+			}
 
-        }
-        return nil
-    })
+		}
+		return nil
+	})
 
-    return nil
+	return nil
 }
 
-
 func rootDir() (root string, err error) {
-    pwd, err := os.Getwd()
-    if err != nil {
-        log.Println("os.Getwd err:", err)
-        return pwd, err
-    }
-    exePath := os.Args[0]
-    parent, _ := path.Split(exePath)
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Println("os.Getwd err:", err)
+		return pwd, err
+	}
+	exePath := os.Args[0]
+	parent, _ := path.Split(exePath)
 
-    var exeDir string
-    if filepath.IsAbs(exeDir) {
-        exeDir = parent
-    } else {
-        exeDir = path.Join(pwd, parent)
-    }
+	var exeDir string
+	if filepath.IsAbs(exeDir) {
+		exeDir = parent
+	} else {
+		exeDir = path.Join(pwd, parent)
+	}
 
+	if dirExists(path.Join(exeDir, "views")) && dirExists(path.Join(exeDir, "public")) {
+		root = exeDir
+	} else {
+		root = pwd
+	}
 
-    if dirExists(path.Join(exeDir, "views")) && dirExists(path.Join(exeDir, "public")) {
-        root = exeDir
-    } else {
-        root = pwd
-    }
-
-    return
+	return
 
 }
 
 func dirExists(dir string) bool {
-    fi, err := os.Stat(dir)
-    if err!= nil {
-        return false
-    }
-    return fi.IsDir()
+	fi, err := os.Stat(dir)
+	if err != nil {
+		return false
+	}
+	return fi.IsDir()
 }
 
 func Run(addr string) {
-    log.Println("=> Girl server start at port: 9999")
-    http.ListenAndServe(addr, &girl)
+	log.Println("=> Girl server start at port: 9999")
+	http.ListenAndServe(addr, &girl)
 }
 
 var girl Girl
+
 func init() {
-    root, _ := rootDir()
-    log.Println("set project root dir:", root)
+	root, _ := rootDir()
+	log.Println("set project root dir:", root)
 
-    routes := make(map[string][]Route)
+	routes := make(map[string][]Route)
 
-    girl = Girl{routes, nil, root}
-    err := girl.initTemplate()
+	girl = Girl{routes, nil, root}
+	err := girl.initTemplate()
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
 }
